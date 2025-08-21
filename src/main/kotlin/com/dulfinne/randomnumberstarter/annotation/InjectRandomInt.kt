67853581f2d59d1c1997c40f1a10750e5a14dd4f
yani <yanazhuchok@gmail.com>
2025-08-21 12:15:35 +0300

    fix: rm min & max bounds from @InjectRandomInt

--- src/main/kotlin/com/dulfinne/randomnumberstarter/annotation/InjectRandomInt.kt
+++ src/main/kotlin/com/dulfinne/randomnumberstarter/annotation/InjectRandomInt.kt
@@ -2,4 +2,4 @@ package com.dulfinne.randomnumberstarter.annotation
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FIELD)
-annotation class InjectRandomInt(val min: Int, val max: Int)
+annotation class InjectRandomInt
